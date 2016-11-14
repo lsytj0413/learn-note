e0a8fc1c40d53e2c06dcdaab438423f1d633a738
soren yang <lsytj0413@users.noreply.github.com>
2016-11-14 21:52:05 +0800

    Update demo.py

--- pyDemo/demo.py
+++ pyDemo/demo.py
@@ -457,6 +457,21 @@ print int2('1000')
 """
 类装饰器
 """
+class Foo(object):
+    def __init__(self, func):
+    self._func = func
+
+    def __call__(self):
+        print ('class decorator runing')
+        self._func()
+        print ('class decorator ending')
+
+@Foo
+def bar():
+    print ('bar')
+
+bar()
+
 
 """
 元类
