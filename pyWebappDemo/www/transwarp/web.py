f790c2806174e725a4bb34fe0aeccc80cc366a07
lsytj0413 <511121939@qq.com>
2016-11-24 20:45:25 +0800

    status 4**

--- pyWebappDemo/www/transwarp/web.py
+++ pyWebappDemo/www/transwarp/web.py
@@ -80,5 +80,29 @@ _RESPONSE_STATUSES = {
     305: 'Use Proxy',
     306: 'Temporary Redirect',
 
-    
+    400: 'Bad Request',
+    401: 'Unauthorized',
+    402: 'Payment Required',
+    403: 'Forbidden',
+    404: 'Not Found',
+    405: 'Method Noe Allowed',
+    406: 'Not Acceptable',
+    407: 'Proxy Authentication Required',
+    408: 'Request Timeout',
+    409: 'Conflict',
+    410: 'Gone',
+    411: 'Length Required',
+    412: 'Precondition Failed',
+    413: 'Request Entity Too Large',
+    414: 'Request URI Too Long',
+    415: 'Unsupported Media Type',
+    416: 'Requested Range Not Satisfiable',
+    417: 'Expectation Failed',
+    418: "I'm a teapot",
+    422: 'Unprocessable Entity',
+    423: 'Locked',
+    424: 'Failed Dependency',
+    426: 'Upgrade Required',
+
+
 }
