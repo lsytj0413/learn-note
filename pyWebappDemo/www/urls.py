32576936618077e370b966a045375a29c587b477
lsytj0413 <511121939@qq.com>
2016-11-28 15:07:52 +0800

    urls

---
+++ pyWebappDemo/www/urls.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+
+"""
+URL处理函数
+"""
+
+from transwarp.web import get, view
+from models import User, Blog, Comment
+
+
+@view('test_users.html')
+@get('/')
+def test_users():
+    users = User.find_all()
+    return dict(users=users)
