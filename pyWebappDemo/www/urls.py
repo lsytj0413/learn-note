4f22a760f1e6ed947ca856c883848af31ee2d4d3
lsytj0413 <511121939@qq.com>
2016-11-30 21:38:02 +0000

    user_register

--- pyWebappDemo/www/urls.py
+++ pyWebappDemo/www/urls.py
@@ -9,6 +9,7 @@ import os, re, time, base64, hashlib, logging
 from transwarp.web import get, post, ctx, view, interceptor, seeother, notfound
 from apis import api, APIError, APIValueError, APIPermissionError, APIResourceNotFountError
 from models import User, Blog, Comment
+from config import configs
 
 
 @view('blogs.html')
@@ -22,7 +23,60 @@ def index():
 @api
 @get('/api/users')
 def api_get_users():
+    """
+    获取用户列表
+    """
     users = User.find_by('order by created_at desc')
     for u in users:
         u.password = '******'
     return dict(users=users)
+
+
+_RE_EMAIL = re.compile(r'^[a-z0-9\.\-\_]+\@[a-z0-9\-\_]+(\.[a-z0-9\-\_]+){1,4}$')
+_RE_MD5 = re.compile(r'^[0-9a-f]{32}$')
+
+
+_COOKIE_NAME = 'awesession'
+_COOKIE_KEY = configs.session.secret
+
+
+def make_signed_cookie(id, password, max_age):
+    """
+    构造一个cookie值
+    """
+    expires = str(int(time.time() + (max_age or 86400)))
+    L = [id, expires, hashlib.md5('%s-%s-%s-%s' % (id, password, expires, _COOKIE_KEY)).hexdigest()]
+    return '-'.join(L)
+
+
+@api
+@post('/api/users')
+def register_user():
+    """
+    用户注册接口
+    """
+    i = ctx.request.input(name='', email='', password='')
+    name = i.name.strip()
+    email = i.email.strip().lower()
+    password = i.password
+
+    if not name:
+        raise APIValueError('name')
+    if not email or not _RE_EMAIL.match(email):
+        raise APIValueError('email')
+    if not password or not _RE_MD5.match(password):
+        raise APIValueError('password')
+
+    user = User.find_first('where email=?', email)
+    if user:
+        raise APIError('register:failed', 'email', 'Email is already in use.')
+
+    user = User(name=name,
+                email=email,
+                password=password,
+                image='http://www.gravatar.com/avatar/%s?d=mm&s=120' % hashlib.md5(email).hexdigest())
+    user.insert()
+
+    cookie = make_signed_cookie(user.id, user.password, None)
+    ctx.response.set_cookie(_COOKIE_NAME, cookie)
+    return user
