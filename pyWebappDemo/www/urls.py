067379e5350d2de78495575646933cd370c83896
lsytj0413 <511121939@qq.com>
2016-12-04 10:40:45 +0000

    Page class

--- pyWebappDemo/www/urls.py
+++ pyWebappDemo/www/urls.py
@@ -193,3 +193,25 @@ def manage_blogs_create():
                 redirect='/manage/blogs',
                 user=ctx.request.user
     )
+
+
+class Page(object):
+    """
+    分页信息存储类
+    """
+
+    def __init__(self, item_count, page_index=1, page_size=10):
+        self.item_count = item_count
+        self.page_size = page_size
+        self.page_count = item_count // page_size + (1 if item_count % page_size > 0 else 0)
+
+        if (0 == item_count) or (page_index < 1) or (page_index > self.page_count):
+            self.offset = 0
+            self.limit = 0
+            self.page_index = 1
+        else:
+            self.page_index = page_index
+            self.offset = self.page_size * (page_index - 1)
+            self.limit = self.page_size
+        self.has_next = self.page_index < self.page_count
+        self.has_previous = self.page_index > 1
